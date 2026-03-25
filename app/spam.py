a55135c1f7996c24a28952b456764df4bb35b5bd
김상록 <gims1480@naver.com>
2026-03-25 21:08:50 +0900

    add keyword2

--- app/spam.py
+++ app/spam.py
@@ -5,7 +5,7 @@ def check_spam(text: str) -> str:
     spam_keywords = [
         "free", "win", "winner", "prize", "click",
         "buy now", "urgent", "cash", "money", "offer", "deal",
-        "guarantee"
+        "guarantee", "limited"
     ]
 
     hit = 0
